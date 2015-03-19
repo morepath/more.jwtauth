54166a0bf210feba5dac3977631dcf08271a3b5d
Henri Schumacher <henri.hulski@gazeta.pl>
2015-03-19 16:42:49 +0100

    Add test_encode_jwt_raw

--- more/jwtauth/tests/test_jwtauth.py
+++ more/jwtauth/tests/test_jwtauth.py
@@ -10,8 +10,6 @@ from webob import Request
 from webob.exc import HTTPForbidden, HTTPProxyAuthenticationRequired
 from webtest import TestApp as Client
 
-import jwt
-
 
 def setup_module(module):
     morepath.disable_implicit()
@@ -257,16 +255,22 @@ def test_authorization():
     assert more.jwtauth.main.get_userid(claims_set_decoded, settings(lookup=lookup).jwtauth) == 'user'
 
 
-def test_encode_jwt():
+def test_encode_jwt_raw():
+    import jwt
+    from sys import version_info
     claims_set = {
         'sub': 'user'
     }
     key = 'secret'
-    token_raw = jwt.encode(claims_set, key)
+    token = jwt.encode(claims_set, key)
+    if version_info >= (3, 0, 0):
+        token = token.decode(encoding='UTF-8')
 
-    assert token_raw == 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyIn0.' \
+    assert token == 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyIn0.' \
                     '8jVjALlPRYpE03sMD8kuqG9D4RSih5NjiISNZ-wO3oY'
 
+
+def test_encode_jwt():
     config = morepath.setup()
     config.scan(more.jwtauth)
 
@@ -276,12 +280,15 @@ def test_encode_jwt():
     @App.setting_section(section="jwtauth")
     def get_jwtauth_settings():
         return {
-            'master_secret': key,
+            'master_secret': 'secret',
             'expiration_delta': None,
         }
 
     config.commit()
     lookup = App().registry.lookup
+    claims_set = {
+        'sub': 'user'
+    }
     token = more.jwtauth.main.encode_jwt(claims_set, settings(lookup=lookup).jwtauth)
 
     assert token == 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyIn0.' \
