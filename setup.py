45c363ba1fe8251f9c46f84f0186b4ec6e4bb9ea
Henri Hulski <henri.hulski@gazeta.pl>
2024-03-03 02:03:20 +0100

    Back to development: 0.15

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (
 
 setup(
     name="more.jwtauth",
-    version="0.14",
+    version="0.15.dev0",
     description="JWT Access Auth Identity Policy for Morepath",
     long_description=long_description,
     author="Morepath developers",
